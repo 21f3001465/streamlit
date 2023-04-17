d4aaec42bde68fb386122d69ab11eddd8559509e
21f3001465 <131007946+21f3001465@users.noreply.github.com>
2023-04-17 21:04:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,60 +2,26 @@
 import streamlit as st
 
 # give a title to our app
-st.title('Welcome to BMI Calculator')
+st.title('App to find greatest of 3 Numbers')
 
-# TAKE WEIGHT INPUT in kgs
-weight = st.number_input("Enter your weight (in kgs)")
+# first number
+first = st.number_input("Enter First Number:")
 
-# TAKE HEIGHT INPUT
-# radio button to choose height format
-status = st.radio('Select your height format: ',
-				('cms', 'meters', 'feet'))
+# second number
+second = st.number_input("Enter Second Number:")
+
+# third number
+third = st.number_input("Enter Third Number:")
 
 # compare status value
-if(status == 'cms'):
-	# take height input in centimeters
-	height = st.number_input('Centimeters')
-
-	try:
-		bmi = weight / ((height/100)**2)
-	except:
-		st.text("Enter some value of height")
-
-elif(status == 'meters'):
-	# take height input in meters
-	height = st.number_input('Meters')
-
-	try:
-		bmi = weight / (height ** 2)
-	except:
-		st.text("Enter some value of height")
-
+if(second > first):
+	if(third > second):
+		Max=third
+	else:
+		Max=second
+elif (third > first):
+	Max=third
 else:
-	# take height input in feet
-	height = st.number_input('Feet')
-
-	# 1 meter = 3.28
-	try:
-		bmi = weight / (((height/3.28))**2)
-	except:
-		st.text("Enter some value of height")
-
-# check if the button is pressed or not
-if(st.button('Calculate BMI')):
-
-	# print the BMI INDEX
-	st.text("Your BMI Index is {}.".format(bmi))
-
-	# give the interpretation of BMI index
-	if(bmi < 16):
-		st.error("You are Extremely Underweight")
-	elif(bmi >= 16 and bmi < 18.5):
-		st.warning("You are Underweight")
-	elif(bmi >= 18.5 and bmi < 25):
-		st.success("Healthy")
-	elif(bmi >= 25 and bmi < 30):
-		st.warning("Overweight")
-	elif(bmi >= 30):
-		st.error("Extremely Overweight")
-
+	Max=first	
+	
+st.success("The greatest of the numbers is {}.".format(Max))
